9f282dd73b26ab8ae4622f536fb204717e32402e
Jan Zhang <ge47wor@mytum.de>
2022-07-14 13:32:23 +0200

    plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -18,8 +18,9 @@ public class QueryProcessor {
         } else if(query.contains("largest")){
             if(query.contains("number")){
                 String input = query.substring(10);
+                String input2 = input.replace("%20"," ");
                 ArrayList<Integer> numbers = new ArrayList<>();
-                for(String numStrng : input.split("[^0-9]+")){
+                for(String numStrng : input2.split("[^0-9]+")){
                     if(!numStrng.isEmpty()) {
                         int test = Integer.parseInt(numStrng);
                         if(test != 20) {
@@ -39,7 +40,8 @@ public class QueryProcessor {
         else if(query.contains("plus")){
             ArrayList<Integer> numbers = new ArrayList<>();
             String input = query.substring(10);
-            for(String numStrng : input.split("[^0-9]+")){
+            String input2 = input.replace("%20"," ");
+            for(String numStrng : input2.split("[^0-9]+")){
                 if(!numStrng.isEmpty()) {
                     int test = Integer.parseInt(numStrng);
                     if(test != 20) {
@@ -58,7 +60,7 @@ public class QueryProcessor {
     public static void main(String[] args) {
         QueryProcessor processor = new QueryProcessor();
         processor.process("largest numbers 10;3123;412 ");
-                System.out.println(processor.process("329ea7c0: what is 15 plus 18"));
+                System.out.println(processor.process("0a82a4a0:%20what%20is%205%20plus%201"));
     }
 
 
